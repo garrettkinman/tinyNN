aaaff27fe1b1a30d0b609013ba4a28874568374a
Garrett Kinman <garrettkinman@gmail.com>
2024-02-02 11:56:44 -0500

    clean up comment

--- tools/lookup_tables.jl
+++ tools/lookup_tables.jl
@@ -9,7 +9,6 @@ function σ(x::UInt8)
     # scale to [-1, 1]
     x_scaled = ((float(x) / 255.0) * 2.0) - 1.0
     y = NNlib.σ(x_scaled)
-    # y = 1.0 / (1.0 + exp(-x_scaled))
     return round(y * 255) |> UInt8
 end
 
