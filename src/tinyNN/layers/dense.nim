d932a15802f057e24b353f096d170d993aba7656
Garrett Kinman <garrettkinman@gmail.com>
2024-02-09 14:59:59 -0500

    basic dense constructor works

--- src/tinyNN/layers/dense.nim
+++ src/tinyNN/layers/dense.nim
@@ -14,23 +14,32 @@ type
         activation*: (Tensor[T]) -> Tensor[T]
 
 # TODO: fix this to work and look nice
-proc newDense*[T](shape: openArray[int], activation: (Tensor[T]) -> Tensor[T]): Dense[T] =
-    Dense[T](
-        weights: rand[T](shape),
-        bias: rand[T](shape[0]), # make sure this is the right shape
+proc newDense*[T](shape: openArray[int], slice: HSlice[T, T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] =
+    result = Dense[T](
+        weights: Tensor[T].rand(shape, slice),
+        bias: Tensor[T].rand([shape[0], 1], slice), # make sure this is the right shape
+        activation: activation
+    )
+
+# TODO: fix this to work and look nice
+proc newDense*[T](weights: Tensor[T], bias: Tensor[T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] =
+    result = Dense[T](
+        weights: weights,
+        bias: bias, # make sure this is the right shape
         activation: activation
     )
 
 # TODO: fix this to work and look nice
 proc newDense*[T](shape: openArray[int], weights: seq[T], bias: seq[T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] =
-    Dense[T](
-        weights: rand[T](shape),
-        bias: rand[T](shape[0]), # make sure this is the right shape
+    result = Dense[T](
+        weights: Tensor[T].new(shape, weights),
+        bias: Tensor[T].new([shape[0], 1], bias), # make sure this is the right shape
         activation: activation
     )
 
-proc new*[T](_: typedesc[Dense[T]], shape: openArray[int], activation: (Tensor[T]) -> void): Dense[T] = newDense[T](shape)
-proc new*[T](_: typedesc[Tensor[T]], shape: openArray[int], weights: seq[T], bias:seq[T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] = newDense(shape, weights, bias, activation)
+proc new*[T](_: typedesc[Dense[T]], shape: openArray[int], slice: HSlice[T, T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] = newDense[T](shape, slice, activation)
+proc new*[T](_: typedesc[Tensor[T]], weights: Tensor[T], bias: Tensor[T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] = newDense(weights, bias, activation)
+proc new*[T](_: typedesc[Tensor[T]], shape: openArray[int], weights: seq[T], bias: seq[T], activation: (Tensor[T]) -> Tensor[T]): Dense[T] = newDense(shape, weights, bias, activation)
 # proc new*[T](_: typedesc[Tensor[T]], shape: openArray[int], value: T): Tensor[T] = newTensor(shape, value)
 
 proc forward*[T](self: Dense, x: Tensor[T]): Tensor[T] =
--- tests/test_layers.nim
+++ tests/test_layers.nim
@@ -3,3 +3,11 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+import unittest
+import tinyNN
+
+let a = Dense[int8].new([2, 3], (1.int8)..(1.int8), relu)
+
+test "dense":
+    # TODO: add tests
+    check a == Dense[int8].new([2, 3], (1.int8)..(1.int8), relu)
