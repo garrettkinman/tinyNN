45432f66aad704418b50082b94d7cd7da0604532
Garrett Kinman <garrettkinman@gmail.com>
2024-10-05 11:59:06 -0400

    removed int8 -- save quantization for later

--- src/tinyNN/layers/softmax.nim
+++ src/tinyNN/layers/softmax.nim
@@ -6,19 +6,8 @@
 import math
 import ../tensors
 
-func exp(x: int8): int8 =
-    # TODO: Implement quantized exp
-    result = 0
-
 func exp(x: float32): float32 =
     result = math.exp(x)
-
-proc exp(tensor: Tensor[int8]): Tensor[int8] =
-    result = Tensor[int8].new(tensor.shape)
-    for it in 0..<tensor.len:
-        let x: int8 = tensor.data[it]
-        let y: int8 = exp(x)
-        result.data[it] = y
 
 proc exp(tensor: Tensor[float32]): Tensor[float32] =
     result = Tensor[float32].new(tensor.shape)
@@ -26,10 +15,6 @@ proc exp(tensor: Tensor[float32]): Tensor[float32] =
         let x: float32 = tensor.data[it]
         let y: float32 = exp(x)
         result.data[it] = y
-
-proc softmax*(tensor: Tensor[int8]): Tensor[int8] =
-    # TODO: Implement quantized softmax
-    result = Tensor[int8].new(tensor.shape)
 
 # TODO: Add dims parameter?
 proc softmax*(tensor: Tensor[float32]): Tensor[float32] =
